66b4d77b1aa62b09f77869f7d98edf5b7125ddbd
Dmitrii Gherastovshi <dgherastovschi@icloud.com>
2022-04-22 21:02:21 +0500

    imported sedona folder for my next project work

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -2,7 +2,7 @@
 (function(window) {
   var helloSpeaker = {};
   var speakWord = "Hello";
-  helloSpeaker.speak = function (name) {
+  helloSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
   }
   window.helloSpeaker = helloSpeaker;
